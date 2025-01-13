d6ee14a22d2ff15b794b3c2d8833d8efd9e87111
tanzie1313 <tanziehickory@gmail.com>
2025-01-12 19:48:33 -0800

    redo fruits app

--- models/fruit.js
+++ models/fruit.js
@@ -1,16 +1,13 @@
-
 // models/fruit.js
 
-// models/fruit.js
-
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 const fruitSchema = new mongoose.Schema({
-  name: String,
-  isReadyToEat: Boolean,
-  price: Number
-});
-const Fruit = mongoose.model("Fruit", fruitSchema); // create model
-// models/fruit.js
+    name: String,
+    isReadyToEat: Boolean,
+  });
+
+  const Fruit = mongoose.model("Fruit", fruitSchema); // create model
+
+  // models/fruit.js
 
 module.exports = Fruit;
--- server.js
+++ server.js
@@ -1,59 +1,57 @@
-// server.js
 
-const dotenv = require("dotenv");
-dotenv.config();
-const express = require("express");
-const mongoose = require("mongoose");
+const dotenv = require("dotenv"); // require package
+dotenv.config(); // Loads the environment variables from .env file
+const mongoose = require("mongoose"); // require package
+// Here is where we import modules
+// We begin by loading Express
+const express = require('express');
 
 const app = express();
-
 // Connect to MongoDB using the connection string in the .env file
 mongoose.connect(process.env.MONGODB_URI);
 // log connection status to terminal on start
 mongoose.connection.on("connected", () => {
-    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-  });
-  // Import the Fruit model
-const Fruit = require("./models/fruit.js");
-app.use(express.urlencoded({ extended: false }));//gives us access o stuff in form in object
-
-
-app.listen(3003, () => {
-  console.log('Listening on port 3003');
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
+// Import the Fruit model
+const Fruit = require("./models/fruit.js");
+app.use(express.urlencoded({ extended: false }));
+
 
 // server.js
-
-// GET /
-// app.get("/", async (req, res) => {
-    // res.send("hello, friend!");
-//   });
-  // server.js
 
 // GET /
 app.get("/", async (req, res) => {
-    res.render("index.ejs");
-  });
-  
-// server.js
+  res.render("index.ejs");
+});
+
+// GET /fruits
+app.get("/fruits", async (req, res) => {
+  const allFruits = await Fruit.find();
+  //console.log(allFruits); // log the fruits
+  res.send("Welcome to the index page!");
+});
+
 
 // GET /fruits/new
 app.get("/fruits/new", (req, res) => {
-    res.render("fruits/new.ejs");
+  res.render("fruits/new.ejs");
 });
-  // server.js
 
 // POST /fruits
-// server.js
-
 // POST /fruits
 app.post("/fruits", async (req, res) => {
-    if (req.body.isReadyToEat === "on") {
-      req.body.isReadyToEat = true;
-    } else {
-      req.body.isReadyToEat = false;
-    }
-    await Fruit.create(req.body);
-    res.redirect("/fruits/new");
-  });
-  //pdate fruit model for price and  fruit form 
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  await Fruit.create(req.body);
+  res.redirect("/fruits/new");
+});
+
+
+
+app.listen(3005, () => {
+  console.log('Listening on port 3005');
+});
